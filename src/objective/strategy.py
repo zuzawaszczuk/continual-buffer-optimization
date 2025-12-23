af66c70ac31689ad2ad64657a72976ea642d1c93
zuzawaszczuk <zuza.waszczuk@gmail.com>
2025-12-23 01:41:00 +0100

    finished optimized buffer strategy

--- src/objective/strategy.py
+++ src/objective/strategy.py
@@ -4,18 +4,18 @@ from typing import Optional, Union, Dict
 import torch
 from torch.nn import Module
 from torch.optim import Optimizer
-from torch.utils.data import DataLoader, Dataset
-from avalanche.benchmarks.scenarios import Experience
+from avalanche.benchmarks.utils import AvalancheDataset, AvalancheConcatDataset
+from avalanche.training.templates import SupervisedTemplate
 from avalanche.training.templates.strategy_mixin_protocol import CriterionType
 
 
-class OptimizedBufferStrategy:
+class OptimizedBufferStrategy(SupervisedTemplate):
     def __init__(
         self,
         model: Module,
         optimizer: Optimizer,
         criterion: CriterionType,
-        task_buffers: Optional[Dict[int, ParametricBuffer]] = None,
+        task_buffers: Optional[Dict[int, AvalancheDataset]] = None,
         train_mb_size: int = 1,
         train_epochs: int = 1,
         eval_mb_size: Optional[int] = None,
@@ -34,15 +34,8 @@ class OptimizedBufferStrategy:
 
     def train_dataset_adaptation(self, dataset: AvalancheDataset) -> AvalancheDataset:
         task_id = self.experience.task_label
-        
-        if task_id in self.task_buffers:
-            buffer_dataset = self.task_buffers[task_id].to_dataset()
-            dataset = AvalancheConcatDataset([buffer_dataset, dataset])
-        
-        return dataset
-    
-    def train(self, experience: Experience) -> None:
-        super().train(experience)
 
-    def eval(self, experience: Experience) -> None:
-        pass
+        if task_id in self.task_buffers:
+            dataset = AvalancheConcatDataset([self.task_buffers[task_id], dataset])
+
+        return dataset
