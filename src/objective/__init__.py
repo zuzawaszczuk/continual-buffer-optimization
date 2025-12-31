b79c7468e0341ba4d319950125a2a0711d434f92
Jakub Jurczak <jurczakjakub01@gmail.com>
2025-12-31 02:07:17 +0100

    implemented neighbors for hill climb

--- src/objective/__init__.py
+++ src/objective/__init__.py
@@ -1,4 +1,4 @@
-from .function import function, get_tasks_buffers
+from .function import Function
 from .manage_benchmark import calculate_mask_length
 
-__all__ = ["calculate_mask_length", "get_tasks_buffers", "function"]
+__all__ = ["calculate_mask_length", "get_tasks_buffers", "Function"]
